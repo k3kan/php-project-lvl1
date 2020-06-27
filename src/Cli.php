e9f49f34e082a29a9fa3fe0185bbf259d5a039ea
k3kan <aintbor@gmail.com>
2020-06-27 12:03:50 +0300

    step 4 modified

--- src/Cli.php
+++ src/Cli.php
@@ -2,18 +2,11 @@
 
 namespace BrainGames\Src\Cli;
 
-$autoloadPath1 = __DIR__ . '/../../../autoload.php';
-$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
-
-if (file_exists($autoloadPath1)) {
-    require_once $autoloadPath1;
-} else {
-    require_once $autoloadPath2;
-}
-
 use function cli\line;
 use function cli\prompt;
 
+/**  function run
+*/
 function run()
 {
     line('Welcome to the Brain Game!');
