bfea5e5a2e9f60fe0bd97bb469baa14714847cf8
K3kan <aintbor@gmail.com>
2020-07-22 23:18:57 +0300

    project 1.1

--- src/games/gcd.php
+++ src/games/gcd.php
@@ -4,18 +4,30 @@ namespace Src\Games\Gcd;
 
 use Brain\Src\Structure;
 
+use const Brain\Src\Structure\MAX_AMOUNT_SOLUTION;
+
 function getMaxDivider()
 {
-    $randomNumberOne = Structure\randomNumber();
-    $randomNumberTwo = Structure\randomNumber();
-    $randomNumberOne < $randomNumberTwo ? $least = $randomNumberOne : $least = $randomNumberTwo;
-    $maxDivider = 1;
-    $question = "Question: {$randomNumberOne} {$randomNumberTwo}";
-    for ($checkDivider = 2; $checkDivider <= $least; $checkDivider++) {
-        if ($randomNumberOne % $checkDivider === 0 && $randomNumberTwo % $checkDivider === 0) {
-            $maxDivider = $checkDivider;
+    for ($solution = 1; $solution  <= MAX_AMOUNT_SOLUTION; $solution++) {
+        $randomNumberOne = Structure\randomNumber();
+        $randomNumberTwo = Structure\randomNumber();
+        $randomNumberOne < $randomNumberTwo ? $least = $randomNumberOne : $least = $randomNumberTwo;
+        $maxDivider = 1;
+        $question = "Question: {$randomNumberOne} {$randomNumberTwo}";
+        for ($checkDivider = 2; $checkDivider <= $least; $checkDivider++) {
+            if ($randomNumberOne % $checkDivider === 0 && $randomNumberTwo % $checkDivider === 0) {
+                $maxDivider = $checkDivider;
+            }
         }
+        $questionAnswer[$question] = (int) $maxDivider;
     }
-    $questionAnswer = [$question, (int) $maxDivider];
     return $questionAnswer;
+}
+
+function getResultGames()
+{
+    $game = getMaxDivider();
+    $introduction = 'Find the greatest common divisor of given numbers.';
+    $nameUser = Structure\askUserName($introduction);
+    $resultGame = Structure\checkTerms($game, $nameUser);
 }
